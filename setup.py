f3b05c75206d41cdaa19474e952de9a940b604d1
RSR <80620448+RSR-TG-Info@users.noreply.github.com>
2025-01-28 17:44:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="zohlathu",
-    version="2.1.5",
+    version="2.1.6",
     author="RSR",
     author_email="imrsrmizo@gmail.com",
     description="A Python package for fetching Mizo song lyrics from Zohlathu.in",
